5ac9d682e9ee0ed15937b11890bc4cec73e3e818
Dominic Midlane <dmidlane@spindogs.com>
2023-03-03 06:31:17 +0000

    assets and minification

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,17 @@
 module.exports = {
     plugins: [
         require('autoprefixer'),
-        // require('postcss-preset-env')({
-        //     stage: 1
-        // }),
+        require('postcss-nested'),
         require('postcss-import'),
         require('postcss-simple-vars'),
-        require('postcss-nested')
+        require('postcss-preset-env')({
+            stage: 1
+        }),
+        require('postcss-assets')({
+            loadPaths: ['dist/img']
+        }),
+        require('cssnano')({
+            preset: 'default'
+        })
     ]
 }
